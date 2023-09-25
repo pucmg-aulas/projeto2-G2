3c78b52f04e777b1d272fef7d313ca18343aa94d
JulyaMoreyra <130570629+JulyaMoreyra@users.noreply.github.com>
2023-09-25 09:43:22 -0300

    Create Equipe.java
    classe equipe

---
+++ codigo/src/Equipe.java
@@ -0,0 +1,18 @@
+public class Equipe {
+    private String nome;
+
+    public Equipe(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String setNome(){
+        return nome;
+    }
+
+
+
+}
