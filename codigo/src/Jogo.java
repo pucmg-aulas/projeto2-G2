205b05a5c4fc86e8fcf77f07581781d7615b85a5
davi <daviferreiradev@gmail.com>
2023-09-26 08:08:25 -0300

    Criar classe Jogo e Equipe

--- codigo/src/Jogo.java
+++ codigo/src/Jogo.java
@@ -1,3 +1,23 @@
+import java.time.LocalDate;
+
 public class Jogo {
-    
+    private LocalDate data;
+    private Equipe equipeA;
+    private Equipe equipeB;
+    private Placar placar;
+
+    public Jogo(LocalDate data, Equipe equipeA, Equipe equipeB, Placar placar) {
+        this.data = data;
+        this.equipeA = equipeA;
+        this.equipeB = equipeB;
+        this.placar = placar;
+    }
+
+    public void exibirPlacar() {
+
+    }
+
+    public char qualEquipe() {
+        
+    }
 }
