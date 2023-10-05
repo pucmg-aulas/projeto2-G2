a7e6906e7998c16f80d44f1068956a71f2f4b049
gramos22 <gabrielrmzf@gmail.com>
2023-10-05 11:34:59 -0300

    feat: add novo construtor em Placar

--- codigo/src/model/Placar.java
+++ codigo/src/model/Placar.java
@@ -14,6 +14,14 @@ public class Placar {
         Q4 = new int[2];
         OT = new int[2];
     }
+
+    public Placar(int[] Q1,int[] Q2,int[] Q3,int[] Q4, int[] OT){
+        this.Q1 = Q1;
+        this.Q2 = Q2;
+        this.Q3 = Q3;
+        this.Q4 = Q4;
+        this.OT = OT;
+    }
     
     public int totalPontos(int posEquipe){
         return Q1[posEquipe] + Q2[posEquipe] + Q3[posEquipe] + Q4[posEquipe] + OT[posEquipe]; 
