5cf33b291a2c0e9710008b3f33b9346fd2dbf978
gramos22 <gabrielrmzf@gmail.com>
2023-10-05 09:56:32 -0300

    refatoracao: add parametro 'ano' em gerar()

--- codigo/src/csv_parsing/gerar/GerarCsvTabela.java
+++ codigo/src/csv_parsing/gerar/GerarCsvTabela.java
@@ -2,6 +2,7 @@ package csv_parsing.gerar;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.time.Year;
 import java.util.ArrayList;
 
 import model.Jogo;
@@ -11,13 +12,13 @@ import model.Tabela;
 
 public class GerarCsvTabela {
 
-    public void gerar(Tabela tabela) {
+    public void gerar(Tabela tabela, Year ano) {
         ArrayList<Jogo> jogos = tabela.getJogos();
         Jogo auxJogo;
         Placar auxPlacar;
         
         try {
-            FileWriter fw = new FileWriter(new File("../db/" + "tabela" + tabela.getJogos().get(0).getData().getYear() + ".csv")); // fileWriter apontando para o path onde estão os dados 
+            FileWriter fw = new FileWriter(new File("../db/" + "tabela" + ano + ".csv")); // fileWriter apontando para o path onde estão os dados 
             
             for(int i = 0; i < jogos.size(); i++) {
                 auxJogo = jogos.get(i);
