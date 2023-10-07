f0a82e8af9112fc21b4d7f4631dfe0f04ce7e842
Ana <anafcs.profissional@gmail.com>
2023-10-07 09:32:42 -0300

    Criação do GerarCsvTemporada

--- codigo/src/csv_parsing/gerar/GerarCsvTemporada.java
+++ codigo/src/csv_parsing/gerar/GerarCsvTemporada.java
@@ -1,5 +1,30 @@
 package csv_parsing.gerar;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.time.Year;
+import java.util.ArrayList;
+
+import model.Temporada;
+import model.Tabela;
+import model.Equipe;
+
+import java.io.BufferedReader;
+import java.io.File;
+
 public class GerarCsvTemporada {
-    
+
+    public void gerar(Temporada temporada, Year ano) {
+        ArrayList<Equipe> equipes = temporada.getEquipes();
+        Tabela tabela = temporada.getTabela; 
+        
+        try {
+            FileWriter fw = new FileWriter(new File("../db/" + "temporada" + ano + ".csv")); // fileWriter apontando para o path onde estão os dados 
+            
+            fw.write (ano.toString() + "," + equipes.get(0).getNome()+ "," + equipes.get(1).getNome()+ ";" + equipes.get(2).getNome()+ ";" + equipes.get(3).getNome()+ ";" + equipes.get(4).getNome()+ ";" + equipes.get(5).getNome()+ ";" + equipes.get(6).getNome()+ ";" + equipes.get(7).getNome()+ "\n")
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
